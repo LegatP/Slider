d9bed4212b0457c24701e0b55dfdafca38a7e43f
Legat Primož <legatprimoz@gmail.com>
2016-06-21 03:54:31 +0000

    Added 'jump to' buttons and slide change animation

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,33 +1,53 @@
 $(document).ready(function() {
     
-    var numberOfSlides = 2;
+    var numberOfSlides = 4;
     var currentSlide = 1;
     var previousSlide = 1;
     
-  $(".changeSlide").click(function () {
-    previousSlide = currentSlide;
-    if($(this).attr('tag') == "next"){
-        currentSlide++;
-        if(currentSlide > numberOfSlides){
-        currentSlide = 1;
-        }
-    }else{
-        currentSlide--;
-        if(currentSlide < 1){
-        currentSlide = numberOfSlides;
-        }        
+    $(".changeSlide").click(function(){
+        changeSlide($(this).attr('tag'));
+        restartTimer();
+    });
+  
+    var slideTimeOut = setInterval(function(){
+        changeSlide("next");
+        },3000);
+        
+    function restartTimer(){
+        clearInterval(slideTimeOut);
+        slideTimeOut = setInterval(function(){
+        changeSlide("next");
+        },3500);
     }
-      
-    $("#slide").html(
-            '<img src="/slides/slide' + currentSlide + '.jpg"></img>' +
-            '<img src="/slides/slide' + previousSlide + '.jpg"></img>'        
-        )
-  });
   
-  
-  
-  
-  
-  
+    function changeSlide(tag){
+        $('.changeSlide').attr('disabled', 'disabled');
+        previousSlide = currentSlide;
+        if(tag == "next"){
+            currentSlide++;
+            if(currentSlide > numberOfSlides){
+            currentSlide = 1;
+            }
+        }else if(tag == "previous"){
+            currentSlide--;
+            if(currentSlide < 1){
+            currentSlide = numberOfSlides;
+            }        
+        }else{
+            currentSlide = tag;
+        }
+          
+        $("#slide").html(
+                '<img style="z-index:1" src="/slides/slide' + currentSlide + '.jpg" />' +
+                '<img style="z-index:2" id="fade" src="/slides/slide' + previousSlide + '.jpg" />'        
+            );
+            
+         $("#fade").animate({
+            opacity:0
+            
+            },1000,function(){
+                $('.changeSlide').removeAttr('disabled');
+            });
+  }
   
 });
