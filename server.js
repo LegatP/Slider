19c7fe94f45d2d9b7a5b60124141cbdd6c508018
Legat Primož <legatprimoz@gmail.com>
2016-07-03 03:45:06 +0000

    View count and basic routing

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 var express = require('express');
-var fs = require('fs');
+var app = express();
+var ejs = require('ejs');
 var path = require('path');
 
+var visits = 0;
 
-var app = express();
-app.set('views', __dirname + '/');
-app.use(express.static(__dirname));
+if(!process.env.PORT)
+    process.env.PORT = 8080;
+    
+  
+var visits = 0;  
+   
+   
+app.use(express.static(path.join(__dirname, 'views')));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
+app.get('/',function(req,res,next){
+    visits++;
+    console.log(visits);
+    res.render('index.ejs');
+})
 
-app.listen(process.env.PORT);
+app.get('/chat',function(req,res){
+    res.render('chat');
+})
+
+app.get('/index',function(req,res){
+    res.render('index.ejs');
+})
+ 
+app.listen(process.env.PORT,function(){
+    console.log("Server listening on PORT " + process.env.PORT);
+});
