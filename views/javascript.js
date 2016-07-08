b221da5fd5945e9af84f4585fadd626c952dbe5e
Legat Primož <legatprimoz@gmail.com>
2016-07-08 13:02:02 +0000

    Added graphics for buttons on slider

--- views/javascript.js
+++ views/javascript.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
     var slideText3 = "Vulputate dictum dolor, id tempus magna elementum quis. Nam non arcu ac tortor commodo ultricies.";
     var slideText4 = "Sed ante velit, scelerisque at massa ac.";    
     
+    var animationRunning = false;
     
     $(".changeSlide").click(function(){
         changeSlide($(this).attr('tag'));
@@ -32,81 +33,82 @@ $(document).ready(function() {
     }
   
     function changeSlide(tag){
-        $('.changeSlide').attr('disabled', 'disabled');
-        
-
-        previousSlide = currentSlide;
-        if(tag == "next"){
-            currentSlide++;
-            if(currentSlide > numberOfSlides){
-            currentSlide = 1;
+       
+       if(animationRunning){
+           /* DO NOTHING, ANIMATION RUNNING*/
+       }else{
+            animationRunning = true;
+            previousSlide = currentSlide;
+            if(tag == "next"){
+                currentSlide++;
+                if(currentSlide > numberOfSlides){
+                currentSlide = 1;
+                }
+            }else if(tag == "previous"){
+                currentSlide--;
+                if(currentSlide < 1){
+                currentSlide = numberOfSlides;
+                }        
+            }else{
+                currentSlide = parseInt(tag,10);
+                
             }
-        }else if(tag == "previous"){
-            currentSlide--;
-            if(currentSlide < 1){
-            currentSlide = numberOfSlides;
-            }        
-        }else{
-            currentSlide = parseInt(tag,10);
             
-        }
-        
-        var slideTitle;
-        var slideText;
-
-        switch(currentSlide){
-            case 1:
-                slideTitle = slideTitle1;
-                slideText = slideText1;
-                break;
-            case 2:
-                slideTitle = slideTitle2;
-                slideText = slideText2;
-                break;
-            case 3:
-                slideTitle = slideTitle3;
-                slideText = slideText3;
-                break;
-            case 4:
-                slideTitle = slideTitle4;
-                slideText = slideText4;
-                break;
-        }    
-          
-          
-        $("#slide").html(
-                '<img style="z-index:1" src="/slides/slide' + currentSlide + '.jpg" />' +
-                '<img style="z-index:2" id="fade" src="/slides/slide' + previousSlide + '.jpg" />'        
+            var slideTitle;
+            var slideText;
+    
+            switch(currentSlide){
+                case 1:
+                    slideTitle = slideTitle1;
+                    slideText = slideText1;
+                    break;
+                case 2:
+                    slideTitle = slideTitle2;
+                    slideText = slideText2;
+                    break;
+                case 3:
+                    slideTitle = slideTitle3;
+                    slideText = slideText3;
+                    break;
+                case 4:
+                    slideTitle = slideTitle4;
+                    slideText = slideText4;
+                    break;
+            }    
+              
+              
+            $("#slide").html(
+                    '<img style="z-index:1" src="/slides/slide' + currentSlide + '.jpg" />' +
+                    '<img style="z-index:2" id="fade" src="/slides/slide' + previousSlide + '.jpg" />'        
+                );
+             $("#fade").animate({
+                opacity:0,
+                },1100,function(){
+                    animationRunning = false;
+            });
+                
+             $("#slideLeft").animate({
+                opacity:0,
+                },100,"linear",function(){
+                    $("#slideLeft").html(slideTitle);
+                    $("#slideLeft").animate({
+                        opacity:1,
+                        },1100
+                    );
+                }
             );
-         $("#fade").animate({
-            opacity:0,
-            },1100,function(){
-                $(".changeSlide").removeAttr("disabled");
-        });
             
-         $("#slideLeft").animate({
-            opacity:0,
-            },100,"linear",function(){
-                $("#slideLeft").html(slideTitle);
-                $("#slideLeft").animate({
-                    opacity:1,
-                    },1100
-                );
-            }
-        );
-        
-         $("#slideRight").animate({
-            opacity:0,
-            },100,"linear",function(){
-                $("#slideRight").html(slideText);
-                $("#slideRight").animate({
-                    opacity:1,
-                    },1100
-                );
-            }
-        );          
-        
-            
+             $("#slideRight").animate({
+                opacity:0,
+                },100,"linear",function(){
+                    $("#slideRight").html(slideText);
+                    $("#slideRight").animate({
+                        opacity:1,
+                        },1100
+                    );
+                }
+            );          
+       }  
   }
   
 });
